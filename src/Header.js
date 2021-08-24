dde5011b321f94da609b4cb12ee2f5f828fbe893
GurkiratSidhu <g3sidhu@gmail.com>
2021-08-24 17:54:22 +0530

    commit

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header() {
 	return (
 		<>
 			<section className="flex flex-col px-0 mt-16 leading-snug text-left dark:text-white md:mx-14">
-				<div className="">
+				<div className="space-y-2">
 					<p className="mt-6 mb-6 text-6xl font-bold md:mt-8 md:mb-8 md:text-5xl text-black-900 dark:text-white-900 ">
 						Hi, I'm Gurkirat!
 					</p>
@@ -13,10 +13,10 @@ function Header() {
 					<p className="text-xl font-extrabold tracking-normal md:text-4xl text-black-700 dark:text-white-700">
 						Javascript/React developer. UX Designer.
 					</p>
-					<p className="text-xl font-extrabold tracking-normal md:text-4xl text-black-700 dark:text-white-700">
+					<p className="mb-4 text-xl font-extrabold tracking-normal md:text-4xl text-black-700 dark:text-white-700">
 						Previously @GyanDhan , @MindScroll and @94Films
 					</p>
-					<div className="flex mt-8 space-x-4 text-4xl dark:text-white">
+					<div className="flex mt-10 space-x-4 text-4xl dark:text-white">
 						<a href="https://github.com/GurkiratSidhu">
 							<FaGithub />
 						</a>
@@ -26,13 +26,16 @@ function Header() {
 					</div>
 				</div>
 
-				<div className="mb-8 text-base md:text-xl dark:text-white-700 text-black-700">
+				<div className="mt-8 mb-8 space-y-4 text-2xl dark:text-white-700 text-black-700">
 					<p>
 						I am a rising React developer. Graduated in CSE from Thapar
-						University, Patiala.<br></br>I enjoy creating high-quality software
-						products with a complex logic behind it. <br></br>My technology
-						stack includes Javascript, React and Tailwind.
+						University, Patiala.
 					</p>
+					<p>
+						I enjoy creating high-quality software products with a complex logic
+						behind it.
+					</p>
+					<p>My technology stack includes Javascript, React and Tailwind.</p>
 				</div>
 			</section>
 		</>
